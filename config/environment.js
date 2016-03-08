656fc2f216e0305d64ef6d9ded0e4b19806d08aa
mike <student@epicodus.com>
2016-03-08 08:25:04 -0800

    install firebase and link database to app

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'shoptivist',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://shoptivist.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
